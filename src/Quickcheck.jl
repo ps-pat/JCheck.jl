ddbc839addada70745a24a0e53e0cb75242ea6c5
Patrick Fournier <p_fournier@hushmail.com>
2022-01-01 12:34:06 -0500

    Move the heavy lifting of @quickcheck to a function.

--- src/Quickcheck.jl
+++ src/Quickcheck.jl
@@ -139,104 +139,105 @@ function Base.show(io::IO, qc::Quickcheck)
     print(header, "\n", join(vars, "\n"))
 end
 
-macro quickcheck(qc)
-    quote
-        local _qc = $(esc(qc))
+function quickcheck(qc::Quickcheck)
+    ## Stores failed cases. The key is the test predicate's
+    ## description (as a symbol). The value is a named tuple
+    ## containing the actual predicate (predicate) and a vector of
+    ## inputs for which the predicate evaluated to `false`
+    ## (valuation).
+    local failed = Dict{Symbol,
+                        NamedTuple{(:predicate, :valuations),
+                                   Tuple{Function, Vector{Tuple}}}}()
 
-        ## Stores failed cases. The key is the test predicate's
-        ## description (as a symbol). The value is a named tuple
-        ## containing the actual predicate (predicate) and a vector of
-        ## inputs for which the predicate evaluated to `false`
-        ## (valuation).
-        local failed = Dict{Symbol,
-                            NamedTuple{(:predicate, :valuations),
-                                       Tuple{Function, Vector{Tuple}}}}()
+    @testset InternalTestSet "Test $desc" for
+        (pred, desc, args) ∈ qc.predicates
 
-        @testset InternalTestSet "Test $desc" for
-            (pred, desc, args) ∈ _qc.predicates
+        ## Flip to `false` if predicate evaluates to `false` for any
+        ## valuation.
+        local holds = true
 
-            ## Flip to `false` if predicate evaluates to `false` for any
-            ## valuation.
-            local holds = true
+        ## Special cases.
+        types = map(v -> qc.variables[v].type, args)
+        specialcases_itr =
+            Iterators.product(specialcases.(types)...) |>
+            Iterators.flatten |>
+            itr -> Iterators.partition(itr, length(args)) |>
+            itr -> Iterators.map(Tuple, itr)
 
-            ## Special cases.
-            types = map(v -> _qc.variables[v].type, args)
-            specialcases_itr =
-                Iterators.product(specialcases.(types)...) |>
-                Iterators.flatten |>
-                itr -> Iterators.partition(itr, length(args)) |>
-                itr -> Iterators.map(Tuple, itr)
+        ## Random cases.
+        randomcases = map(arg -> qc.variables[arg].values, args)
 
-            ## Random cases.
-            randomcases = map(arg -> _qc.variables[arg].values, args)
+        for valuation ∈ Iterators.flatten([specialcases_itr,
+                                           zip(randomcases...)])
+            pass = pred(valuation...)
 
-            for valuation ∈ Iterators.flatten([specialcases_itr,
-                                               zip(randomcases...)])
-                pass = pred(valuation...)
+            if !pass
+                ex = Expr(:tuple,
+                          (Expr(:(=), arg, val)
+                           for (arg, val) ∈ zip(args, valuation))...)
+                @warn "Predicate \"$desc\" does not hold \
+                               for valuation $ex"
 
-                if !pass
-                    ex = Expr(:tuple,
-                              (Expr(:(=), arg, val)
-                               for (arg, val) ∈ zip(args, valuation))...)
-                    @warn "Predicate \"$desc\" does not hold \
-                           for valuation $ex"
-
-                    if _qc.serialize_fails
-                        if !haskey(failed, Symbol(desc))
-                            failed[Symbol(desc)] = (predicate = pred,
-                                                    valuations = Tuple[])
-                        end
-
-                        push!(failed[Symbol(desc)].valuations, valuation)
+                if qc.serialize_fails
+                    if !haskey(failed, Symbol(desc))
+                        failed[Symbol(desc)] = (predicate = pred,
+                                                valuations = Tuple[])
                     end
 
-                    holds = false
+                    push!(failed[Symbol(desc)].valuations, valuation)
                 end
+
+                holds = false
             end
-            @test holds
         end
-
-        if !isempty(failed) && _qc.serialize_fails
-            filename = "JCheck_" * Dates.format(Dates.now(),
-                                                "yyyy-mm-dd_HH-MM-SS")
-            fileextension = ".jchk"
-
-            ## Make sure that no 2 files with the same name are
-            ## created. Most likely useless except for people with
-            ## nothing better to do trying to break shit.
-            if ispath(filename * fileextension)
-                filename *= "--1"
-            end
-
-            while ispath(filename * fileextension)
-                ## 29 (28) works because the part of the name up to
-                ## the index has a fixed width. Please don't fuck it
-                ## up.
-                idx = parse(Int, filename[29:end]) + 1
-
-                filename = filename[1:28] * string(idx)
-            end
-
-            JLSO.save(filename * fileextension, failed)
-
-            ## Warn the user that some predicates do not hold.
-            message_color = Base.warn_color()
-
-            println()
-            printstyled("Some predicates do not hold for some valuations; \
-                         they have been saved to \
-                         $(filename * fileextension). Use function ",
-                        color = message_color)
-            printstyled("load", color = message_color, underline = true)
-            printstyled(" and macro ", color = message_color)
-            printstyled("@getcases",
-                        color = message_color,
-                        underline = true)
-            printstyled(" to explore the problematic cases.",
-                        color = message_color)
-            println("\n")
-        end
-
-        nothing
+        @test holds
     end
+
+    if !isempty(failed) && qc.serialize_fails
+        filename = "JCheck_" * Dates.format(Dates.now(),
+                                            "yyyy-mm-dd_HH-MM-SS")
+        fileextension = ".jchk"
+
+        ## Make sure that no 2 files with the same name are
+        ## created. Most likely useless except for people with
+        ## nothing better to do trying to break shit.
+        if ispath(filename * fileextension)
+            filename *= "--1"
+        end
+
+        while ispath(filename * fileextension)
+            ## 29 (28) works because the part of the name up to
+            ## the index has a fixed width. Please don't fuck it
+            ## up.
+            idx = parse(Int, filename[29:end]) + 1
+
+            filename = filename[1:28] * string(idx)
+        end
+
+        JLSO.save(filename * fileextension, failed)
+
+        ## Warn the user that some predicates do not hold.
+        message_color = Base.warn_color()
+
+        println()
+        printstyled("Some predicates do not hold for some valuations; \
+                             they have been saved to \
+                             $(filename * fileextension). Use function ",
+                    color = message_color)
+        printstyled("load", color = message_color, underline = true)
+        printstyled(" and macro ", color = message_color)
+        printstyled("@getcases",
+                    color = message_color,
+                    underline = true)
+        printstyled(" to explore the problematic cases.",
+                    color = message_color)
+        println("\n")
+    end
+
+    nothing
+end
+
+macro quickcheck(qc)
+    _qc = esc(qc)
+    :(quickcheck($_qc))
 end
