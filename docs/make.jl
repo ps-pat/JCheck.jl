ead890cc6610063f55ed9f3483dd39fbe518c0df
Patrick Fournier <p_fournier@hushmail.com>
2022-01-02 16:26:40 -0500

    Actual documentation & GitHub workflow.

---
+++ docs/make.jl
@@ -0,0 +1,15 @@
+using JCheck
+
+using Documenter
+
+DocMeta.setdocmeta!(JCheck,
+                    :DocTestSetup,
+                    :(using JCheck),
+                    recursive=true)
+
+makedocs(sitename="JCheck.jl",
+         doctest = true,
+         modules = [JCheck])
+
+deploydocs(
+    repo = "github.com/ps-pat/JCheck.jl.git")
---
+++ docs/src/index.md
@@ -0,0 +1,15 @@
+# JCheck.jl Documentation
+
+```@meta
+CurrentModule = JCheck
+```
+
+```@docs
+Quickcheck
+Quickcheck(::AbstractString)
+```
+
+``` @docs
+@quickcheck(::Any)
+```
+
