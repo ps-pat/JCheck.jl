fc25028a9c0a666bc712622aea988ae75b393c19
Patrick Fournier <p_fournier@hushmail.com>
2022-01-05 16:22:12 -0500

    Typo.

--- src/Quickcheck.jl
+++ src/Quickcheck.jl
@@ -325,7 +325,7 @@ function quickcheck(qc::Quickcheck, file_id::AbstractString)
 end
 
 """
-    @quickcheck qc [file_id::AbstractString=yyyy-mm-dd_HH-MM-SS]
+    @quickcheck qc [file_id::AbstractString="yyyy-mm-dd_HH-MM-SS"]
 
 Check the properties specified in object `qc` of type [`Quickcheck`](@ref).
 
