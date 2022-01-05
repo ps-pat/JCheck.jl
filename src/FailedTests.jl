fc29f9bff5f664a2ff2f5809ad6572142c614898
Patrick Fournier <p_fournier@hushmail.com>
2022-01-05 17:16:12 -0500

    Examples for macro `@getcases`.

--- src/FailedTests.jl
+++ src/FailedTests.jl
@@ -65,6 +65,17 @@ failed.
 The predicate with description closest to the one given (in the sense of
 the Levenshtein distance) will be returned; there is no need to pass the
 exact description.
+
+# Examples
+``` jldoctest
+julia> ft = JCheck.load("JCheck_test.jchk")
+2 failing predicates:
+Product commute
+Is odd
+
+julia> pred, valuations = @getcases ft iod
+NamedTuple{(:predicate, :valuations), Tuple{Function, Vector{Tuple}}}((Serialization.__deserialized_types__.var"#3#4"(), Tuple[(0,), (-9223372036854775808,), (6444904272543528628,)]))
+```
 """
 macro getcases(ft, desc...)
     _ft = esc(ft)
