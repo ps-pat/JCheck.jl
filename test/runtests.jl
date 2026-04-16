4fdafd592e7e62aa0286dd991443968e51b418a2
Patrick Fournier <pf@patrickfournier.ca>
2026-04-16 10:26:28 -0400

    test: move `Aqua.test_all` inside main `@testset`

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,11 @@ using Test
 
 import Aqua
 
-Aqua.test_all(JCheck)
-
 include("types_with_generate.jl")
 
 @testset "JCheck's tests" begin
+    Aqua.test_all(JCheck)
+
     include("Quickcheck.jl")
     include("shrink.jl")
     include("generate.jl")
